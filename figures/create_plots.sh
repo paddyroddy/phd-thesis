20f5f1594238a871e2e59b92298f5e4ea40c85f1
paddyroddy <patrickjamesroddy@gmail.com>
2021-07-29 10:10:21 +0100

    Create one folder per chapter

--- figures/create_plots.sh
+++ figures/create_plots.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-bash scripts/chapter2/chapter2.sh
-bash scripts/chapter3/chapter3.sh
-bash scripts/chapter4/chapter4.sh
+for i in {2..4}; do
+    echo chapter: $i
+    ./chapter$i/chapter$i.sh
+done
