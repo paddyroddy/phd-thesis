d4832747c6d914b98ba76ef8ff01667a225df78c
paddyroddy <patrickjamesroddy@gmail.com>
2021-07-29 11:31:15 +0100

    Create master figures script

--- figures/chapter4/chapter4.sh
+++ figures/chapter4/chapter4.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+B=3
+J_MIN=2
+J_RANGE=$(seq 0 4)
+MATPLOTLIB_PLOTS=$HOME/project/src/s2sleplet/pys2sleplet/plotting/mesh
 MESH=homer
+OTHER_MESHES=(
+    bird
+    cheetah
+    cube
+    dragon
+    teapot
+)
 P_RANGE=(
     0
     9
@@ -9,23 +20,85 @@ P_RANGE=(
     199
 )
 RANK_RANGE=$(seq 2 9)
+SIGMA = $(seq 1 2)
+SNR=-10
 
 for r in ${RANK_RANGE[@]}; do
-    echo figure: , r: $r
+    echo figure: 2, r: $r
     mesh $MESH \
         -e $r
 done
 
-echo figure:
+echo figure: 3
+python \
+    $MATPLOTLIB_PLOTS\mesh_tiling.py \
+    $MESH
+
+echo figure: 4
+python \
+    $MATPLOTLIB_PLOTS\mesh_region.py \
+    $MESH
+
+for p in ${P_RANGE[@]}; do
+    echo figure: 5, p: $p
+    mesh $MESH \
+        -m slepian \
+        -e $p \
+        -u
+done
+
+echo figure: 6
+python \
+    $MATPLOTLIB_PLOTS\mesh_slepian_eigenvalues.py \
+    $MESH
+
+echo figure: 7, field
 mesh $MESH \
     -m field
-echo figure:
+echo figure: 7, Slepian field
 mesh $MESH \
     -m slepian_field
 
-for p in ${P_RANGE[@]}; do
-    echo figure: , p: $p
+echo figure: 8, scaling
+mesh $MESH \
+    -m wavelets \
+    -u
+echo figure: 9, scaling
+mesh $MESH \
+    -m coefficients \
+    -u
+
+for j in $J_RANGE; do
+    echo figure: 8, j: $j
     mesh $MESH \
-        -m slepian \
-        -e $p
+        -e $B $J_MIN $j \
+        -m wavelets \
+        -u
+
+    echo figure: 9, j: $j
+    mesh $MESH \
+        -e $B $J_MIN $j \
+        -m coefficients \
+        -u
+done
+
+echo figure: 10, noised
+mesh $MESH \
+    -m slepian_field \
+    -n $SNR \
+    -u
+
+for s in ${SIGMA[@]}; do
+    echo figure: 10, sigma: $s
+    python \
+        $MATPLOTLIB_PLOTS/denoising_slepian_mesh.py \
+        -n $SNR \
+        -s $s
+done
+
+for f in ${OTHER_MESHES[@]}; do
+    echo figure: 11, mesh: $f
+    python \
+        $MATPLOTLIB_PLOTS\mesh_region.py \
+        $f
 done
