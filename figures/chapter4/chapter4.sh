785fdc9b642726021cb8652062768879b70efd5a
paddyroddy <patrickjamesroddy@gmail.com>
2021-09-23 15:36:38 +0100

    Include a pic of cotangent Laplacian

--- figures/chapter4/chapter4.sh
+++ figures/chapter4/chapter4.sh
@@ -24,23 +24,23 @@ SIGMA=1
 SNR=-5
 
 for r in ${RANK_RANGE[@]}; do
-    echo figure: 1, r: $r
+    echo figure: 2, r: $r
     mesh $MESH \
         -e $r
 done
 
-echo figure: 3
+echo figure: 4
 python \
     $MATPLOTLIB_PLOTS/mesh_tiling.py \
     $MESH
 
-echo figure: 4
+echo figure: 5
 python \
     $MATPLOTLIB_PLOTS/mesh_region.py \
     $MESH
 
 for p in ${P_RANGE[@]}; do
-    echo figure: 5, p: $p
+    echo figure: 6, p: $p
     mesh $MESH \
         -m slepian \
         -e $p \
@@ -48,36 +48,31 @@ for p in ${P_RANGE[@]}; do
         -z
 done
 
-echo figure: 6
+echo figure: 7
 python \
     $MATPLOTLIB_PLOTS/mesh_slepian_eigenvalues.py \
     $MESH
 
-echo figure: 8, field
-mesh $MESH \
-    -m field \
-    -u
-
-echo figure: 7, scaling
+echo figure: 8, scaling
 mesh $MESH \
     -m wavelets \
     -u \
     -z
-echo figure: 9, scaling
+
+echo figure: 9, field
 mesh $MESH \
-    -m coefficients \
-    -u \
-    -z
+    -m field \
+    -u
 
 for j in $J_RANGE; do
-    echo figure: 7, j: $j
+    echo figure: 8, j: $j
     mesh $MESH \
         -e $B $J_MIN $j \
         -m wavelets \
         -u \
         -z
 
-    echo figure: 9, j: $j
+    echo figure: 10, j: $j
     mesh $MESH \
         -e $B $J_MIN $j \
         -m coefficients \
@@ -85,20 +80,26 @@ for j in $J_RANGE; do
         -z
 done
 
-echo figure: 10, original
+echo figure: 10, scaling
+mesh $MESH \
+    -m coefficients \
+    -u \
+    -z
+
+echo figure: 11, original
 mesh $MESH \
     -m slepian_field \
     -u \
     -z
 
-echo figure: 10, noised
+echo figure: 11, noised
 mesh $MESH \
     -m slepian_field \
     -n $SNR \
     -u \
     -z
 
-echo figure: 10, denoised
+echo figure: 11, denoised
 python \
     $MATPLOTLIB_PLOTS/denoising_slepian_mesh.py \
     $MESH \
@@ -106,7 +107,7 @@ python \
     -s $SIGMA
 
 for f in ${OTHER_MESHES[@]}; do
-    echo figure: 11, mesh: $f
+    echo figure: 12, mesh: $f
     python \
         $MATPLOTLIB_PLOTS/mesh_region.py \
         $f
