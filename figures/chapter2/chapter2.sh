8e05ca4072c06fc2e2ed9778718e796878803d6b
paddyroddy <patrickjamesroddy@gmail.com>
2021-09-27 18:15:45 +0100

    Start adding figures on the north pole and translation

--- figures/chapter2/chapter2.sh
+++ figures/chapter2/chapter2.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 ALPHA=0.75
 BETA=0.125
+FUNCTIONS=(gaussian elongated_gaussian squashed_gaussian)
 L=128
 L_SIGMA=$(seq 1 2)
 
-echo figure: 2
-sphere earth \
-    -L $L \
-    -t $TYPE
+for f in $FUNCTIONS; do
+    echo figure: $f north
+    sphere $f \
+        -L $L
+
+    echo figure: $f translated
+    sphere $f \
+    -a $ALPHA \
+    -b $BETA \
+    -m translate \
+    -o
+done
 
 for ell in $L_SIGMA; do
-    echo figure: 1, l_sigma: $ell
+    echo figure: harmonic Gaussian north, l_sigma: $ell
+    sphere harmonic_gaussian \
+        -e $ell 1 \
+        -L $L
+
+    echo figure: harmonic Gaussian translated, l_sigma: $ell
     sphere harmonic_gaussian \
         -a $ALPHA \
         -b $BETA \
         -e $ell 1 \
         -L $L \
         -m translate \
-        -o \
-        -t $TYPE
+        -o
 
-    echo figure: 3, l_sigma: $ell
+    echo figure: harmonic Gaussian convolved, l_sigma: $ell
     sphere harmonic_gaussian \
         -c earth \
-        -e $ell 1 -L $L \
-        -t $TYPE
+        -e $ell 1 \
+        -L $L
 done
