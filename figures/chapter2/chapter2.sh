3c461e534e0e9d228a0f360ac7bdb79682e987c8
paddyroddy <patrickjamesroddy@gmail.com>
2021-10-15 17:43:39 +0100

    Add axisymmetric wavelets plots

--- figures/chapter2/chapter2.sh
+++ figures/chapter2/chapter2.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+B=3
+ELL_RANGE=$(seq 0 4)
+J_MIN=2
+J_RANGE=$(seq 0 3)
 L=128
-l_max=4
 PLOTS=$HOME/project/src/s2sleplet/pys2sleplet/plotting/polar_cap
 
-for l in $(seq 0 $l_max); do
+echo scaling function
+sphere axisymmetric_wavelets \
+    -L 128 \
+    -u
+
+for j in $J_RANGE; do
+    echo wavelet j: $j
+    sphere axisymmetric_wavelets \
+        -e $B $J_MIN $j \
+        -L $L \
+        -u
+done
+
+for l in $ELL_RANGE; do
     for m in $(seq 0 $l); do
         echo l: $l, m: $m
         sphere spherical_harmonic \
--- figures/chapter4/chapter4.sh
+++ figures/chapter4/chapter4.sh
@@ -2,7 +2,7 @@
 B=3
 J_MIN=2
 J_RANGE=$(seq 0 4)
-L_ARBITRARY=128
+L=128
 MATPLOTLIB_PLOTS=$HOME/project/src/s2sleplet/pys2sleplet/plotting/arbitrary
 P_RANGE=(
     0
@@ -20,73 +20,65 @@ SIGMA=(
 SMOOTHING=2
 SNR=-10
 
-echo figure: 2
+echo tiling
 python $MATPLOTLIB_PLOTS/tiling.py
 
-echo figure: 3, Earth
+echo Earth
 sphere earth \
-    -L $L_ARBITRARY \
+    -L $L \
     -s $SMOOTHING \
-    -t $TYPE \
     -u
-echo figure: 3, Slepian South America
+echo Slepian South America
 sphere slepian_south_america \
-    -L $L_ARBITRARY \
+    -L $L \
     -s $SMOOTHING \
-    -t $TYPE \
     -u
 
 for p in ${P_RANGE[@]}; do
-    echo figure: 4, p: $p
+    echo Slepian p: $p
     sphere slepian \
         -e $p \
-        -L $L_ARBITRARY \
-        -t $TYPE \
+        -L $L \
         -u
 done
 
-echo figure: 5
+echo eigenvalues
 python $MATPLOTLIB_PLOTS/eigenvalues_south_america.py
 
-echo figure: 6, scaling
+echo scaling wavelet
 sphere slepian_wavelets \
-    -L $L_ARBITRARY \
-    -t $TYPE \
+    -L $L \
     -u
-echo figure: 7, scaling
+echo scaling wavelet coefficient
 sphere slepian_wavelet_coefficients \
-    -L $L_ARBITRARY \
+    -L $L \
     -s $SMOOTHING \
-    -t $TYPE \
     -u
 
 for j in $J_RANGE; do
-    echo figure: 6, j: $j
+    echo wavelet j: $j
     sphere slepian_wavelets \
         -e $B $J_MIN $j \
-        -L $L_ARBITRARY \
-        -t $TYPE \
+        -L $L \
         -u
 
-    echo figure: 7, j: $j
+    echo wavelet coefficient j: $j
     sphere slepian_wavelet_coefficients \
         -e $B $J_MIN $j \
-        -L $L_ARBITRARY \
+        -L $L \
         -s $SMOOTHING \
-        -t $TYPE \
         -u
 done
 
-echo figure: 8, noised
+echo fnoised
 sphere slepian_south_america \
-    -L $L_ARBITRARY \
+    -L $L \
     -n $SNR \
     -s $SMOOTHING \
-    -t $TYPE \
     -u
 
 for s in ${SIGMA[@]}; do
-    echo figure: 8, sigma: $s
+    echo denoised sigma: $s
     python \
         $MATPLOTLIB_PLOTS/denoising_slepian_south_america.py \
         -n $SNR \
